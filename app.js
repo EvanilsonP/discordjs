a7fa20535bd02f9fc2849cd6be98619b41924c67
EvanilsonP <pevanilson99@gmail.com>
2023-05-15 11:03:55 -0300

    Listening and Handling Events

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-require('dotenv').config();
-const { Client } = require('discord.js');
+import { config } from 'dotenv';
+import { Client } from 'discord.js';
+config(); // // Grabbing environment variables
 
-const client = new Client({ intents: ['Guilds', 'GuildMessages']});
+const client = new Client({ intents: ['Guilds', 'GuildMessages', 'MessageContent']});
+
+// Showing the client's tag
+client.on('ready', (client) => {
+    console.log(`${client.user.tag} is online. ✔️ `);
+});
+
+client.on('messageCreate', (message) => {
+    if(message.content.toLocaleLowerCase() === 'coffee') {
+        message.reply('Sorry! I have ran out of coffee already! :(');
+    };
+});
 
 // Logging in
 client.login(process.env.TOKEN);
